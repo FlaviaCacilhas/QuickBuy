0753b0bf75f17033358abf300a542b101a338c61
FlaviaCacilhas <flaviarcacilhas@gmail.com>
2019-09-25 14:20:17 +0100

    Primeiras Entidades

---
+++ QuickBuy.Domain/Entities/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace QuickBuy.Domain.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DatePedido { get; set; }
+        public int UserId { get; set; }
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+
+    }
+}
---
+++ QuickBuy.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Name { get; set; }
+        public string SobreNome { get; set; }
+
+        // Um usuario pode ter nem um ou muitos pedidos
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
